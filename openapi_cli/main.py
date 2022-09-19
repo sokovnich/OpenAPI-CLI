ca770e5b67fbd05e325692ec0313d8229be5953a
Yan Sokovnich <x6@live.ru>
2022-09-20 00:22:51 +0300

    add support for multiple specs
    too long to complete

--- openapi_cli/main.py
+++ openapi_cli/main.py
@@ -26,7 +26,6 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 assert BASE_URL, 'OPENAPI_CLI_BASE_URL env must be defined and contain root API endpoint'
 assert SPEC_URLS != [''], 'OPENAPI_CLI_SPEC_URLS env must be defined and contain OpenAPI spec urls separated by a semicolon'
-assert len(SPEC_URLS) <= 1, 'Multiple spec urls are not supported yet'
 
 
 def main():
@@ -34,23 +33,21 @@ def main():
     session.verify = VERIFY
 
     with Cache() as cache:
-        spec_url = SPEC_URLS[0]
-
         if cache.auth.get('kwargs'):
             session.auth = Decs3O(**cache.auth['kwargs'])
         else:
             session.auth = Decs3O.from_creds_user_input(verify=VERIFY)
             cache.auth['kwargs'] = session.auth.kwargs
 
-        spec = cache.spec.get(spec_url) or cache.spec.setdefault(
-            spec_url, session.get(spec_url).json()
-        )
+        specs = []
+        for spec_url in SPEC_URLS:
+            specs.append(
+                cache.spec.get(spec_url) or cache.spec.setdefault(
+                    spec_url, session.get(spec_url).json()
+                )
+            )
 
-        base_path = spec['basePath']
-        endpoints = spec['paths']
-        tags = {tag['name']: tag['description'] for tag in spec.get('tags', [])}
-
-        args = parse_args(endpoints=endpoints)
+        args = parse_args(specs=specs)
         if getattr(args, 'dry_run', False):
             sys.exit()
         elif getattr(args, 'invalidate_cache', False):
@@ -58,10 +55,7 @@ def main():
             cache.invalidate()
             sys.exit()
 
-        url = urlparse.urljoin(
-            urlparse.urljoin(BASE_URL, base_path),
-            '/'.join(name for i, name in sorted(vars(args.chain).items()))
-        )
+        url = urlparse.urljoin(BASE_URL, args.path)
 
         response = session.request(method=args.method, url=url, params=vars(args.kwargs) if hasattr(args, 'kwargs') else None)
 
--- openapi_cli/parser.py
+++ openapi_cli/parser.py
@@ -1,7 +1,11 @@
 import argparse
 
+import six
 
-def parse_args(endpoints):
+urlparse = six.moves.urllib.parse
+
+
+def parse_args(specs):
     main_parser = ArgumentParser(add_help=True)
     main_container = main_parser.add_subparsers(help='API entry points')
     main_container.required = False
@@ -15,43 +19,58 @@ def parse_args(endpoints):
         required=True,
     )
 
-    entities = {}
-    for endpoint, endpoint_spec in endpoints.items():
-        parsed_endpoint = (
-            tuple() if endpoint == '/'
-            else tuple(endpoint.split('/'))[1:]
-        )
-        container = main_container
-        for item_id, item in enumerate(parsed_endpoint):
-            container.dest = 'chain.i{}'.format(item_id)
-            entity_name = parsed_endpoint[:item_id + 1]
-            entity = entities.get(entity_name)
-            if entity:
-                container = entity
-            else:
-                parser = container.add_parser(item, help=item)
-                container = parser.add_subparsers(
-                    title='{} subcommands'.format(item),
-                    description=None,
-                    help='Item description:',
-                )
-                container.required = True
-                entities[entity_name] = container
+    base_path_map = {}
+    for spec in specs:
+        base_path = spec.get('basePath', '/')
+        endpoints = spec['paths']
+        tags = {tag['name']: tag['description'] for tag in spec.get('tags', [])}
 
-        container.dest = 'method'
+        entities = {}
+        for endpoint, endpoint_spec in endpoints.items():
+            parsed_endpoint = (
+                tuple() if endpoint == '/'
+                else tuple(endpoint.split('/'))[1:]
+            )
+            base_path_map[parsed_endpoint] = base_path
 
-        for method, method_spec in endpoint_spec.items():
-            parser = container.add_parser(method, help=method_spec.get('summary'))
-            group = parser.add_argument_group('kwargs')
-            for param in method_spec.get('parameters', []):
-                group.add_argument(
-                    '--{}'.format(param['name']),
-                    help=param.get('description'),
-                    required=param.get('required', False),
-                    dest='kwargs.{}'.format(param['name'])
-                )
+            container = main_container
+            for item_id, item in enumerate(parsed_endpoint):
+                container.dest = 'chain.i{}'.format(item_id)
+                entity_name = parsed_endpoint[:item_id + 1]
+                entity = entities.get(entity_name)
+                if entity:
+                    container = entity
+                else:
+                    parser = container.add_parser(item, help=item)
+                    container = parser.add_subparsers(
+                        title='{} subcommands'.format(item),
+                        description=None,
+                        help='Item description:',
+                    )
+                    container.required = True
+                    entities[entity_name] = container
 
-    return main_parser.parse_args(namespace=Namespace())
+            container.dest = 'method'
+
+            for method, method_spec in endpoint_spec.items():
+                parser = container.add_parser(method, help=method_spec.get('summary'))
+                group = parser.add_argument_group('kwargs')
+                for param in method_spec.get('parameters', []):
+                    group.add_argument(
+                        '--{}'.format(param['name']),
+                        help=param.get('description'),
+                        required=param.get('required', False),
+                        dest='kwargs.{}'.format(param['name'])
+                    )
+
+    args = main_parser.parse_args(namespace=Namespace())
+    endpoint = tuple(name for i, name in sorted(vars(args.chain).items()))
+    args.path = urlparse.urljoin(
+        base_path_map[endpoint],
+        '/'.join(endpoint)
+    )
+
+    return args
 
 
 class ArgumentParser(argparse.ArgumentParser):
